c344deba719134c5191c0cb8c46961b882e8221e
Jonathon Powell <jonathonwpowell@gmail.com>
2019-11-12 00:13:09 -0700

    coded  in chunked transfer header

--- expressServer/server.js
+++ expressServer/server.js
@@ -61,7 +61,8 @@ router.get("/bus.pb", (req,res,next) => {
         // console.log(feedMessage.entity[0]);
         // console.log("----------------------------------------")
         //console.log(decodedMessage.entity);
-        res.set("Content-Length",encodedMessage.length.toString());
+        //res.set("Content-Length",encodedMessage.length.toString());
+        res.set("Transfer-Encoding","chunked");
         res.write(encodedMessage);
         res.end();
         //console.log("Sent data. Content Length " + encodedMessage.length);
