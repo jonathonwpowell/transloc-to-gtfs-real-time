363083b42a923a39ce886cf36c6bb615bb68a25a
Jonathon Powell <jonathonwpowell@gmail.com>
2019-11-12 17:21:21 -0700

    Cleaned up the code and made the agencyId generic

--- expressServer/server.js
+++ expressServer/server.js
@@ -6,13 +6,55 @@ var app = express();
 
 const router = express.Router();
 
-router.get("/bus.pb", (req,res,next) => {
+function createVehiclePos(busData) {
+    var position = new GtfsRealtimeBindings.transit_realtime.Position({
+        "latitude": busData.location.lat,
+        "longitude": busData.location.lng
+    })
+
+    var vehicleDesc = new GtfsRealtimeBindings.transit_realtime.VehicleDescriptor({
+        "id": busData.vehicle_id,
+    });
+
+    var vehiclePos = new GtfsRealtimeBindings.transit_realtime.VehiclePosition({
+        position,
+        "timestamp": timestampToUnix(busData.last_updated_on),
+        vehicle: vehicleDesc,
+    });
+
+    return vehiclePos;
+}
+
+function createFeedHeader(timestamp) {
+    var feedHeader = new GtfsRealtimeBindings.transit_realtime.FeedHeader({
+        "gtfs_realtime_version": "2.0",
+        "incrementality": 0,
+        "timestamp": timestampToUnix(timestamp)
+    });
+
+    return feedHeader;
+}
+
+function timestampToUnix(timestamp) {
+    return new Date(timestamp).getTime() / 1000;
+}
+
+router.get("/here", (a,b,c) => {
+    console.log("22");
+    b.end("a");
+})
+
+router.get("/vehiclepositions/:agencyId(\\d+)", (req,res,next) => {
+    var agencyId = req.params.agencyId;
+    if (!agencyId) {
+        throw new Error("Agency ID must be defined");
+    }
 
     var busCall = unirest("GET", "https://transloc-api-1-2.p.rapidapi.com/vehicles.json");
-
+    
     busCall.query({
         "callback": "call",
-        "agencies": "16"
+        "agencies": agencyId
     });
 
     busCall.headers({
@@ -20,53 +62,26 @@ router.get("/bus.pb", (req,res,next) => {
         "x-rapidapi-key": "IcmLKZZ5zEmshFRGhk5AZLEqKIN8p1EhU12jsnox1jlqBKWleK"
     });
 
-
     busCall.end(function (busRes) {
         if (busRes.error) throw new Error(busRes.error);
         var feedMessage = new GtfsRealtimeBindings.transit_realtime.FeedMessage();
-        feedMessage.header = new GtfsRealtimeBindings.transit_realtime.FeedHeader({
-            "gtfs_realtime_version": "2.0",
-            "incrementality": 0,
-            "timestamp": new Date(busRes.body.generated_on).getTime() / 1000
-        });
+        feedMessage.header = createFeedHeader(busRes.body.generated_on);
 
-        var buses = busRes.body["data"]["16"];
+        var buses = busRes.body["data"][agencyId];
         buses.forEach((bus) => {
-            var position = new GtfsRealtimeBindings.transit_realtime.Position({
-                "latitude": bus.location.lat,
-                "longitude": bus.location.lng
-            })
 
-
-            var vehicleDesc = new GtfsRealtimeBindings.transit_realtime.VehicleDescriptor({
-                "id": bus.vehicle_id,
-            });
-
-            var vehiclePos = new GtfsRealtimeBindings.transit_realtime.VehiclePosition({
-                position,
-                "timestamp": new Date(bus.last_updated_on).getTime() / 1000,
-                vehicle: vehicleDesc,
-            })
-
+            var vehiclePos = createVehiclePos(bus);
 
             var feedEntity = new GtfsRealtimeBindings.transit_realtime.FeedEntity({"vehicle": vehiclePos, "id": bus.vehicle_id});
             feedMessage.entity.push(feedEntity);
-            // console.log("Is Vehicle pos valid: " + GtfsRealtimeBindings.transit_realtime.VehiclePosition.verify(vehiclePos));
-            // console.log("Is pos valid: " + GtfsRealtimeBindings.transit_realtime.Position.verify(position));
-            // console.log("Is feedEntity valid: " + GtfsRealtimeBindings.transit_realtime.FeedEntity.verify(feedEntity));
         });
 
         var encodedMessage = GtfsRealtimeBindings.transit_realtime.FeedMessage.encode(feedMessage).finish();
-        //var decodedMessage = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(encodedMessage);
-        // console.log(feedMessage.entity[0]);
-        // console.log("----------------------------------------")
-        //console.log(decodedMessage.entity);
-        //res.set("Content-Length",encodedMessage.length.toString());
         res.set("Content-Type","application/x-protobuf");
         
         //res.set("Transfer-Encoding","chunked");
         res.end(encodedMessage);
-        //console.log("Sent data. Content Length " + encodedMessage.length);
+        console.log("Sent data for agency " + agencyId + " at " + new Date().toISOString());
     });
 });
 app.use("/.netlify/functions/server",router);
