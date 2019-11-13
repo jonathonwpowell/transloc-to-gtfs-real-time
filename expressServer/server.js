a5d7a0246aa98d9ef2e25109d94bb3b86db15b71
Jonathon Powell <jonathonwpowell@gmail.com>
2019-11-12 20:35:01 -0700

    Content Length to string

--- expressServer/server.js
+++ expressServer/server.js
@@ -78,7 +78,7 @@ router.get("/vehiclepositions/:agencyId(\\d+)", (req,res,next) => {
 
         var encodedMessage = GtfsRealtimeBindings.transit_realtime.FeedMessage.encode(feedMessage).finish();
         res.set("Content-Type","application/x-protobuf");
-        res.set("Content-Length",encodedMessage.length);
+        res.set("Content-Length",encodedMessage.length.toString());
         //res.set("Transfer-Encoding","chunked");
         res.end(encodedMessage);
         console.log("Sent data of size " + encodedMessage.length + " for agency " + agencyId + " at " + new Date().toISOString());
